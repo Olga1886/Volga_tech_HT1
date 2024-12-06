60afa2838443cc0fd61e720c2a091deb27ee3e8c
Olga1886 <bliznec1886@gmail.com>
2024-12-06 21:20:02 +0300

    Update HW3_task1.py
    исправление кода согласно корректировкам преподавателя

--- HW3_task1.py
+++ HW3_task1.py
@@ -19,16 +19,31 @@ def word_dictionary(data) -> dict:
             frequency_dic[word] += 1
     return frequency_dic
 
+
+def split_to_chapters(data):
+    # Разбиваем текст на главы
+    deliminators_indicis = [i for i, e in enumerate(data) if e == '[new chapter]']
+    start = 0
+    chapters = []
+    for index in deliminators_indicis:
+        chapter_context = data[start + 1:index]
+        chapters.append(chapter_context)
+        start = index
+    last_chapter = data[start + 1:]
+    chapters.append(last_chapter)
+    return chapters
+
+
 def create_chapters_dicts(data):
     deliminators_indicis = [i for i, e in enumerate(data[1:]) if e == '[new chapter]']
     start = 0
     chapter_frequencies = []
     for index in deliminators_indicis:
-        chapter_context = data[start:index]
+        chapter_context = data[start + 1:index]
         chapter_frequency = word_dictionary(chapter_context)
         chapter_frequencies.append(chapter_frequency)
         start = index
-    last_chapter = data[start:]
+    last_chapter = data[start +1:]
     chapter_frequency = word_dictionary(last_chapter)
     chapter_frequencies.append(chapter_frequency)
     return chapter_frequencies
@@ -108,16 +123,18 @@ import math
 from collections import Counter
 
 def get_tf_idf(data, target_word: str, target_chapter: int) -> float:
-    chapter_text = data[target_chapter]
-    words = chapter_text.lower().split()
-    total_words = len(words)
-    term_count = words.count(target_word.lower())
+    # Разделение документа на главы
+    chapter_texts = split_to_chapters(data)  # создание списка глав
+    chapter_text = chapter_texts[target_chapter]
+    print(chapter_text)
+    total_words = len(chapter_text)
+    term_count = chapter_text.count(target_word.lower())
     if total_words > 0:
         tf = term_count / total_words
     else:
         tf = 0.0
-    total_chapters = len(data)
-    doc_count = sum(1 for chapter in data if target_word.lower() in chapter.lower())
+    total_chapters = len(chapter_texts)
+    doc_count = sum(1 for chapter in chapter_texts if target_word.lower() in chapter)
     if doc_count > 0:
         idf = math.log(total_chapters / doc_count)
     else:
@@ -125,13 +142,19 @@ def get_tf_idf(data, target_word: str, target_chapter: int) -> float:
     tf_idf = tf * idf
     return tf_idf
 
-#Пример:
-data = read_data("war_peace.txt")
+# Пример использования:
+file_path = "war_peace.txt"
+data = read_data(file_path)
 target_word = "князь"
-target_chapter = 1
-tf_idf_score = get_tf_idf(data, target_word, target_chapter)
+target_chapter = 95
+tf_idf_score = get_tf_idf(file_path, target_word, target_chapter)
+
+
 print(f"TF-IDF for '{target_word}' in chapter {target_chapter}: {tf_idf_score}")
 
-print(f"TF-IDF for 'свет' in chapter 20: {tf_idf_score}")
-print(f"TF-IDF for 'Пьер' in chapter 20: {tf_idf_score}")
-print(f"TF-IDF for 'дуб' in chapter 110: {tf_idf_score}")
+# Вывод TF-IDF для других слов и глав:
+#print(f"TF-IDF for 'свет' in chapter 20: {get_tf_idf(file_path, 'свет', 20)}")
+#print(f"TF-IDF for 'Пьер' in chapter 20: {get_tf_idf(file_path, 'Пьер', 20)}")
+#print(f"TF-IDF for 'дуб' in chapter 110: {get_tf_idf(file_path, 'дуб', 110)}")
+
+#для корректной работы вызова функции в последнем задании необходимо поставить # перед другими примерами в задачах
